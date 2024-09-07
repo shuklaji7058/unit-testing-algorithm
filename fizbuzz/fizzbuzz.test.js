000e4f59a523737ad35f9dcd38f5d54c8105f562
Anish Shukla <shuklaanish040@gmail.com>
2024-09-07 20:08:50 +0530

    first commit

--- fizbuzz/fizzbuzz.test.js
+++ fizbuzz/fizzbuzz.test.js
@@ -4,4 +4,9 @@ describe("fizzbuzz", () => {
   it("should be a function", () => {
     expect(typeof fizzBuzz).toEqual("function");
   });
+  it("should return the number if not divisible by 3 or 5", () => {
+    expect(fizzBuzz(1)).toEqual(1);
+    expect(fizzBuzz(13)).toEqual(13);
+    expect(fizzBuzz(17)).toEqual(17);
+  });
 });
