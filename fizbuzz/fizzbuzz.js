a13991e574b55d9bd9fc32936afed2e7607e95e3
Anish Shukla <shuklaanish040@gmail.com>
2024-09-07 20:03:42 +0530

    first commit

---
+++ fizbuzz/fizzbuzz.js
@@ -0,0 +1,11 @@
+function fizzBuzz(number) {
+  if (number % 3 === 0) {
+    return "Fizz";
+  } else if (number % 5 === 0) {
+    return "Buzz";
+  } else {
+    return number;
+  }
+}
+
+module.exports = fizzBuzz;
